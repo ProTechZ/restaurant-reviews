dbd66c9713e9883aad7b6a8ed095501c1dd73fb6
Zion Pulapakura <kingofbeastz777@gmail.com>
2024-09-15 19:31:40 +1000

    set up /predict route on fastapi

--- fastapi-server/server.py
+++ fastapi-server/server.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from ipynb.fs.full.model import predict
+
 
 app = FastAPI()
 
 @app.get('/')
 async def root():
-  return {}
+  return {"message": "home page 🥳🥳"}
+
+class Review(BaseModel):
+  review: str
+
+
+@app.post('/predict')
+async def _(body: Review):
+  liked = predict(body.review)
+  return {"liked": str(liked)}
